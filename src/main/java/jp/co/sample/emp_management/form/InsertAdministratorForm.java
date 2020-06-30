12b33643da79db33eaf2760c8e1541ef72357ed6
Yu-Konishi <yu.konishi@rakus.co.jp>
2020-06-30 15:51:00 +0900

    (1-2)入力値エラー処理を追加。 　　 エラーが出た際は入力した文字はフォームに残るように変更

--- src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
@@ -1,5 +1,9 @@
 package jp.co.sample.emp_management.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,10 +12,15 @@ package jp.co.sample.emp_management.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message = "名前を入力してください")
 	private String name;
 	/** メールアドレス */
+	@Email(message = "メールアドレスが不正です")
+	@NotBlank(message = "メールアドレスを入力してください")
 	private String mailAddress;
 	/** パスワード */
+	@NotBlank(message = "パスワードを入力してください")
+	@Size(min = 8, max = 32, message = "パスワードは8文字以上32文字以下にしてください")
 	private String password;
 	public String getName() {
 		return name;
